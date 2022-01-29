47b506094cf077cecb45d8d3db8dc6554b7996a5
Ricardo Gonzales <rzamarron20@unm.edu>
2022-01-29 15:35:30 -0700

    Implemented and added a canvas

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+    GUI gui = new GUI();
+    Scene scene = new Scene(gui,640,480);
+    stage.setScene(scene);
+    stage.show();
+
+    gui.draw();
+    }
+
+    public static void main(String[] args) {
+    launch();
+
+    }
+
 }
